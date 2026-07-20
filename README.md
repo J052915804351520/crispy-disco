f96335d28bdd8f30bf1676f5d99a076280781e48
J052915804351520 <gayathri0414m@gmail.com>
2026-07-20 16:52:23 +0000

    update dice roll bot

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # crispy-disco
-This is a tiny slash command bot that responds to /roll-a-dice /
+This is a tiny slash command bot that responds to /roll-a-dice /flip /8ball
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print["Hello World!"] 
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+# Initializes your app with your bot token
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+# Simple test command: responds when someone says "hello"
+@app.message("hello")
+def message_hello(message, say):
+    say(f"Hey there <@{message['user']}>!")
+
+if __name__ == "__main__":
+    # Starts your app in Socket Mode so it listens forever
+    handler = SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN"))
+    handler.start()
+
